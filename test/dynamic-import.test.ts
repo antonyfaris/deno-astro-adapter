2e788ab7bfa2812b9c93d1dfdd29ffa676022176
Antony Faris <acp325@gmail.com>
2024-03-23 23:20:16 -0500

    Update dynamic import helper function

--- test/dynamic-import.test.ts
+++ test/dynamic-import.test.ts
@@ -2,12 +2,12 @@
 /* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
 import { DOMParser } from 'https://deno.land/x/deno_dom@v0.1.35-alpha/deno-dom-wasm.ts';
 import { assert, assertEquals } from 'https://deno.land/std@0.158.0/testing/asserts.ts';
-import { runBuildAndStartApp } from './helpers.ts';
+import { runBuildAndStartAppFromSubprocess } from './helpers.ts';
 
 Deno.test({
 	name: 'Dynamic import',
 	async fn(t) {
-		const app = await runBuildAndStartApp('./fixtures/dynimport/');
+		const app = await runBuildAndStartAppFromSubprocess('./fixtures/dynimport/');
 
 		await t.step('Works', async () => {
 			const resp = await fetch(app.url);
--- test/helpers.ts
+++ test/helpers.ts
@@ -51,11 +51,60 @@ export async function startModFromImport(baseUrl: URL): Promise<ExitCallback> {
 	return () => mod.stop();
 }
 
+export async function startModFromSubprocess(baseUrl: URL): Promise<ExitCallback> {
+	const entryUrl = new URL('./dist/server/entry.mjs', baseUrl);
+	// const proc = Deno.run({
+	// 	cmd: ['deno', 'run', '--allow-env', '--allow-net', fromFileUrl(entryUrl)],
+	// 	cwd: fromFileUrl(baseUrl),
+	// 	stderr: 'piped',
+	// });
+
+	const proc = createCommand(
+		[
+			"deno",
+			"run",
+			"--allow-env",
+			"--allow-net",
+			fromFileUrl(entryUrl),
+		],
+		{
+			cwd: fromFileUrl(baseUrl),
+			stderr: "piped",
+		},
+	).spawn();
+
+
+	const stderr = proc.stderr;
+	const dec = new TextDecoder();
+	for await (const bytes of stderr) {
+		const msg = dec.decode(bytes);
+		if (msg.includes(`Server running`)) {
+			break;
+		}
+	}
+
+	return () => {
+		// await proc.stdin?.close();
+		// await proc.status; // Ensure any pending operation completes
+		// proc.kill();
+		proc.kill();
+	};
+}
+
 export async function runBuildAndStartApp(fixturePath: string) {
 	const url = new URL(fixturePath, dir);
 
 	await runBuild(fixturePath);
 	const stop = await startModFromImport(url);
+
+	return { url: defaultURL, stop };
+}
+
+export async function runBuildAndStartAppFromSubprocess(fixturePath: string) {
+	const url = new URL(fixturePath, dir);
+
+	await runBuild(fixturePath);
+	const stop = await startModFromSubprocess(url);
 
 	return { url: defaultURL, stop };
 }
